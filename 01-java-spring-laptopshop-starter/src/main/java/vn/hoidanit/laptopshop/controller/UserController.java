2e02cd4c757a03b3a3d3c8f7bd4ff174cd8bd322
tedvkieu <dvkieu165@gmail.com>
2024-11-05 20:55:03 +0700

    Set Up JSP... cuc kho nhat tung lam

--- 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,32 +1,30 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import vn.hoidanit.laptopshop.service.UserService;
-
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 
-@Controller // day la annotation
+
+
+
+@Controller
 public class UserController {
 
-        //DI: dependency Injection;
-    private UserService userService;
-    
-    public UserController(UserService userService) {
-        this.userService = userService;
+    // private final UserService userService;
+
+    // public UserController(UserService userService) {
+    //     this.userService = userService;
+    // }
+
+
+    @RequestMapping("home")
+    public String getHomePage() {
+
+        return "index";
     }
 
-    @RequestMapping("/") // day la annotation de gui request (yeu cau)
-    public String getHomePage() { // chỉ đưa tên của file, để nó hiểu. Nên định dạng nó là String
-        String test = this.userService.handleHello();
-        return "eric.html";
-    }
 }
 // @RestController // day la annotation
 // public class UserController {
