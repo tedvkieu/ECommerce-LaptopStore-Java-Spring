16d5722899d9a78fc78fa63e9a95e14598b9fa17
tedvkieu <dvkieu165@gmail.com>
2024-11-07 20:55:08 +0700

    Repository

--- 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
@@ -16,14 +15,14 @@ public class UserController {
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("home")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        String hihi = "Day la chuoi de test";
-        model.addAttribute("test", test);
-        model.addAttribute("kieu", hihi);
+
+        model.addAttribute("test", "test");
+        model.addAttribute("kieu", "hihi");
 
         return "index";
     }
@@ -36,11 +35,10 @@ public class UserController {
 
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User tedvkieu) {
-        String test = this.userService.handleHello();
-        String hihi = "Day la chuoi de test";
-        model.addAttribute("test", test);
-        model.addAttribute("kieu", hihi);
+
         System.out.println("run here" + tedvkieu);
+
+        this.userService.handleSaveUser(tedvkieu);
         return "index";
     }
 
