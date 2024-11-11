157d278145f41f1af3004e59ef2dd781ea32cea6
tedvkieu <dvkieu165@gmail.com>
2024-11-11 17:07:33 +0700

    mo hinh hoa Data Spring JPA

--- 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -15,59 +15,54 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
-
+    private String avatar;
     public long getId() {
         return id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getFullName() {
         return fullName;
     }
-
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
+    public String getAvatar() {
+        return avatar;
+    }
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
+    
 }
