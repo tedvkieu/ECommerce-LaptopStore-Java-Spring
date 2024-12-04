cdd2bdade5bd4d1012a97292878115133a411aeb
tedvkieu <dvkieu165@gmail.com>
2024-12-04 15:02:49 +0700

    101 VAlidate Model

--- 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
+++ 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/controller/admin/UserController.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.validation.Valid;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UploadService;
 import vn.hoidanit.laptopshop.service.UserService;
@@ -66,8 +70,13 @@ public class UserController {
 
     @PostMapping(value = "/admin/user/create")
     public String createUserPage(Model model,
-            @ModelAttribute("newUser") User tedvkieu,
-            @RequestParam("imgUser") MultipartFile file) {
+            @ModelAttribute("newUser") @Valid User tedvkieu,
+            @RequestParam("imgUser") MultipartFile file, BindingResult bindingResult) {
+
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        for (FieldError error : errors) {
+            System.out.println(error.getObjectName() + " - " + error.getDefaultMessage());
+        }
 
         String avatar = this.uploadService.handleSaveUploadFile(file, "avatar");
 
--- 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ 01-java-spring-laptopshop-starter/src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -2,8 +2,6 @@ package vn.hoidanit.laptopshop.domain;
 
 import java.util.List;
 
-import org.hibernate.annotations.ManyToAny;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +10,10 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -19,13 +21,21 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull
+    @Email
     private String email;
+
+    @NotNull
+    @Min(3)
     private String password;
+
+    @NotNull
+    @Min(2)
     private String fullName;
     private String address;
     private String phone;
     private String avatar;
-
 
     @ManyToOne
     @JoinColumn(name = "role_id")
@@ -33,8 +43,6 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     List<Order> orders;
-
-    
 
     public Role getRole() {
         return role;
